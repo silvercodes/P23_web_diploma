0fbba8c3759e0268491a869afd624d3b5c8c75f5
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-05-24 20:09:14 +0300

    add: oop

--- 1.php_base/src/5.streams.php
+++ 1.php_base/src/5.streams.php
@@ -1,3 +1,61 @@
 <?php
 
+//file_put_contents('file.txt', 'hello');
+//
+//$content = file_get_contents('data.txt');
+//echo $content;
 
+
+//file_put_contents('file.txt', 'hello');
+//file_put_contents('file.txt', 'world', FILE_APPEND);
+//$content = file_get_contents('file.txt');
+//echo $content;
+
+
+//$handle = fopen('data.txt', 'w');
+//fwrite($handle, "Vasia\n");
+//fwrite($handle, "Petya\n");
+//fwrite($handle, "Dima\n");
+//fclose($handle);
+
+
+//$handle = fopen('data.txt', 'r');
+//while (!feof($handle)) {
+//  $line = fgets($handle);
+//  echo $line . '<br>';
+//}
+//fclose($handle);
+//
+//unlink('data.txt');
+
+
+
+
+//echo 'Enter your name: ';
+//$name = trim(fgets(STDIN));
+//echo "Hello $name!";
+
+
+//$stream = fopen('php://memory', 'r+');
+//fwrite($stream, 'Data in memory\n');
+//rewind($stream);
+//echo stream_get_contents($stream);
+//fclose($stream);
+
+
+
+
+//file_exists();
+//is_file();
+//is_dir();
+//is_writable();
+//is_readable();
+//
+//filesize();
+//filetime();
+//
+//mkdir();
+//rmdir();
+//unlink();
+//copy();
+//rename();
---
+++ 2.oop/src/1.class.php
@@ -0,0 +1,301 @@
+<?php
+
+//class User
+//{
+//  public string $name;    // Это свойство :-)
+//  public string $email;
+//
+//  public function __contruct() {
+//
+//  }
+//
+//  public function test(): string {
+//    return "Hello $this->name";
+//  }
+//}
+//
+//$user = new User();
+//$user->name = 'Vasia';
+//echo $user->test();
+//
+//
+//class Connection
+//{
+//  private $host;
+//  private bool $connected = false;
+//
+//  public function __construct($host = 'localhost') {
+//    $this->host = $host;
+//    $this->connect();
+//  }
+//  private function connect() {
+//    echo "Connection to {$this->host}\n}";
+//  }
+//  public function __destruct() {
+//    //
+//    //
+//  }
+//}
+
+
+// =========== promoted properties (FROM 8.0)
+//class User
+//{
+//  public function __construct(
+//    public string $name,
+//    public string $email
+//  )
+//  {}
+//}
+
+//class User
+//{
+//  public function __construct(
+//    public readonly string $name,
+//    public string $email
+//  )
+//  {}
+//}
+
+
+// ========= Property hooks FROM 8.4 =======
+//class User
+//{
+//  private string $_email;
+//  public string $email {
+//    get => $this->_email;
+//    set {
+//      $this->_email = $value;
+//    }
+//  }
+//}
+
+
+//class User
+//{
+//  private string $_email;
+//  public string $email {
+//    get => $this->_email;
+//    set {
+//      if (! filter_var($value, FILTER_VALIDATE_EMAIL)) {
+//        throw new \InvalidArgumentException("Invalid email");
+//      }
+//      $this->_email = $value;
+//    }
+//  }
+//}
+//
+//$user = new User();
+//$user->email = 'test@test.com';
+//$user->email = 'qwerty';
+
+
+
+//class Rect
+//{
+//  public function __construct(
+//    public float $w,
+//    public float $h,
+//  ) {}
+//
+//  public float $area {
+//    get => $this->w * $this->h;
+//  }
+//}
+//
+//$rect = new Rect(100, 200);
+//echo $rect->area;
+
+
+
+// =========== INHERITANCE ==========
+
+//class Unit
+//{
+//  protected string $title;
+//  public function __construct(string $title) {
+//    $this->title = $title;
+//  }
+//  public function info(): string {
+//    return 'INFO';
+//  }
+//  final public function getTitle(): string {
+//    return $this->title;
+//  }
+//}
+//
+//class Archer extends Unit
+//{
+//  public function getLevel(): int {
+//    return 10;
+//  }
+//  public function info(): string {
+//    $baseInfo = parent::info();
+//    return "$baseInfo Archer INFO";
+//  }
+//}
+//
+//$archer = new Archer('vasia');
+//
+//echo $archer->info();
+
+
+
+// ============== Abstract class =================
+
+//abstract class Shape
+//{
+//  protected string $color = 'default_color';
+//  public function __construct(string $color) {
+//    $this->color = $color;
+//  }
+//  abstract public function getArea(): float;
+//  public function getColor(): string {
+//    return $this->color;
+//  }
+//}
+//
+//class Rect extends Shape
+//{
+//  private float $w;
+//  private float $h;
+//  public function __construct(string $color, float $w, float $h) {
+//    parent::__construct($color);
+//    $this->w = $w;
+//    $this->h = $h;
+//  }
+//
+//  public function getArea(): float {
+//    return $this->w * $this->h;
+//  }
+//}
+//
+//$rect = new Rect('red', 3, 4);
+//echo $rect->getArea();
+
+
+
+
+// ================= static ================
+
+//class User {
+//  public static $prop;
+//  public static function execute() {}
+//}
+//
+//echo User::execute();
+//
+//class Counter {
+//  private static $count = 0;
+//
+//  public function __construct() {
+//    self::$count++;
+//    echo "Created: " . self::$count . "\n";
+//  }
+//  public static function getCount() {
+//    return self::$count;
+//  }
+//}
+
+
+
+
+// ===========  $this   self::  static::  parent:: =============
+// self::       Compile-time      Класс, где написан код                      Наследование игнорируется     Раннее связывание
+// static::     Runtime           Класс, через который произвели вызов        Наследование учитывается      Позднее связывание
+// parent::     Compile-time      Непосредственный родитель                   Только родитель
+
+//class ParentClass {
+//  public static function who() {
+//    echo __CLASS__;
+//  }
+//  public static function test() {
+//    self::who();
+//  }
+//}
+//
+//class Child extends ParentClass {
+//  public static function who() {
+//    echo __CLASS__;
+//  }
+//}
+//
+//Child::test();
+
+
+
+
+//class ParentClass {
+//  public static function who() {
+//    echo __CLASS__;
+//  }
+//  public static function test() {
+//    static::who();
+//  }
+//}
+//
+//class Child extends ParentClass {
+//  public static function who() {
+//    echo __CLASS__;
+//  }
+//}
+//
+//Child::test();
+//ParentClass::test();
+
+
+
+// -- Singleton --
+
+class Database
+{
+  private static $instances = [];
+
+  protected function __construct() {}
+
+  public static function getInstance() {
+    $class = static::class;       // Получение вызвавшего класса
+
+    if (! isset(self::$instances[$class])) {
+      self::$instances[$class] = new static();
+    }
+
+    return self::$instances[$class];
+  }
+}
+
+class MySQLDatabase extends Database {}
+class PostgresQLDatabase extends Database {}
+
+$mysql = MySQLDatabase::getInstance();
+$pgsql = PostgresQLDatabase::getInstance();
+
+
+
+// ---- fabric ------
+
+class Model {
+  public static function create($data): static
+  {
+    $instance = new static();
+
+    foreach ($data as $key => $value) {
+      $instance->{$key} = $value;
+    }
+
+    return $instance;
+  }
+}
+
+class User extends Model {}
+class Post extends Model {}
+
+$user = User::create(['name' => 'Vasia']);
+$post = Post::create(['title' => 'Hello']);
+
+
+
+
+
+
+
