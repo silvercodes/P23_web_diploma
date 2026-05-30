44c5a39e82ad02f2b2e4d7df8d8492a1c1dad785
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-05-30 23:01:26 +0300

    add: magic

---
+++ 2.oop/src/3.magic.php
@@ -0,0 +1,270 @@
+<?php
+
+// ========== __get __set ============
+
+///**
+// * class User
+// *
+// * @property string $status
+// */
+//class User {
+//  /**
+//   * @var array
+//   */
+//  private array $data = [];
+//
+//  /**
+//   * @param string $name
+//   * @return mixed|null
+//   */
+//  public function __get(string $name) {
+//    echo "__get $name\n";
+//    return $this->data[$name] ?? null;
+//  }
+//
+//  /**
+//   * @param string $name
+//   * @param mixed $value
+//   * @return void
+//   */
+//  public function __set(string $name, mixed $value): void {
+//    echo "__set $name: $value\n";
+//    $this->data[$name] = $value;
+//  }
+//}
+//
+//$user = new User();
+//$user->status = 'active';
+//echo $user->status."\n";
+
+
+
+
+//class Product {
+//  private float $price;
+//
+//  public function __get(string $name) {
+//    return match($name) {
+//      'priceWithVal' => $this->price * 1.2,
+//      'priceWithoutVal' => $this->price * 0.8,
+//      default => null,
+//    };
+//  }
+//  public function __set(string $name, $value): void {
+//    if ($name === 'price') {
+//      $this->price = $value;
+//    }
+//  }
+//}
+//
+//$p = new Product();
+//$p->price = 100;
+//echo $p->priceWithVal;
+
+
+
+
+// =========== __call / __callStatic ============
+
+//class Router {
+//  public function __call(string $name, array $args) {
+//    echo "Calling method $name\n";
+//    $argsAsString = array_map(function($arg) {
+//      return is_array($arg) ? json_encode($arg) : (string)$arg;
+//    }, $args);
+//
+//    echo "Args: " . implode(', ', $argsAsString) . "\n";
+//  }
+//}
+//
+//$router = new Router();
+//$router->get('/users');
+//$router->post('/users', [1, 2, 3]);
+//$router->post('/data', [
+//  'id' => 101,
+//  'name' => 'John Doe',
+//  'roles' => [
+//    'admin', 'author'
+//  ]
+//]);
+
+
+
+
+//class User {
+//  private array $attributes = [];
+//
+//  public static function __callStatic(string $name, array $arguments)
+//  {
+//    if (! str_starts_with($name, 'create')) {
+//      throw new Exception("Method $name does not allowed");
+//    }
+//
+//    $user = new self();
+//    $user->attributes['name'] = $args[0] ?? null;
+//    return $user;
+//  }
+//}
+//
+//$user = User::create(['Vasia']);
+
+
+
+
+// ============= __isset __unset =========
+
+//class Model {
+//  private array $data = [];
+//
+//  public function __isset(string $name): bool {
+//    echo "Checking if $name is set";
+//
+//    return isset($this->data[$name]);
+//  }
+//
+//  public function __set(string $name, mixed $value): void {
+//    $this->data[$name] = $value;
+//  }
+//
+//  public function __unset(string $name): void {
+//    echo "Unset $name";
+//    unset($this->data[$name]);
+//  }
+//}
+//
+//$model = new Model();
+//$model->name = 'Vasia';
+//
+//var_dump(isset($model->name));
+//var_dump(isset($model->email));
+//
+//unset($model->name);
+//var_dump(isset($model->name));
+
+
+
+
+
+
+// ============= __toString / __invoke ==========
+
+//interface IStrategy {
+//  public function __invoke(array $data): array;
+//}
+//
+//class UpperCaseStrategy implements IStrategy {
+//  public function __invoke(array $data): array {
+//    return array_map('strtoupper', $data);
+//  }
+//}
+//
+//class LowerCaseStrategy implements IStrategy {
+//  public function __invoke(array $data): array {
+//    return array_map('strtolower', $data);
+//  }
+//}
+//
+//class Processor {
+//  public function process(array $data, IStrategy $strategy) {
+//    return $strategy($data);
+//  }
+//}
+//
+//$p = new Processor();
+//var_dump($p->process(['a', 'b', 'c'], new UpperCaseStrategy()));
+
+
+
+
+// =============== QueryBuilder ==================
+
+class QueryBuilder {
+  private array $select = ['*'];
+  private string $from = '';
+  private array $wheres = [];
+  private array $orderBy = [];
+  private int|null $limit = null;
+
+  public function select(array $fields): self {
+    $this->select = $fields;
+    return $this;
+  }
+  public function from(string $table): self {
+    $this->from = $table;
+    return $this;
+  }
+
+  public function __call(string $name, array $args): self {
+    if (str_starts_with($name, 'where')) {
+      $field = lcfirst(substr($name, 5));
+      $this->wheres[] = [
+        'field' => $field,
+        'value' => $args[0] ?? null,
+      ];
+
+      return $this;
+    }
+
+    if ($name === 'orderBy') {
+      $this->orderBy[] = $args[0] ?? null;
+      return $this;
+    }
+
+    if ($name === 'limit') {
+      $this->limit = $args[0] ?? null;
+      return $this;
+    }
+
+    throw new Exception("Method $name() does not exist");
+  }
+
+  public function getSql(): string {
+    $select = implode(', ', $this->select);
+    $from = $this->from;
+
+    $whereString = '';
+    if (! empty($this->wheres)) {
+      $conditions = array_map(
+        fn($w) => "{$w['field']} = '{$w['value']}'",
+        $this->wheres);
+
+      $whereString = ' WHERE ' . implode(' AND ', $conditions);
+    }
+
+    $orderString = '';
+    if (! empty($this->orderBy)) {
+      $orderString = ' ORDER BY ' . implode(', ', $this->orderBy);
+    }
+
+    $limitString = '';
+    if ($this->limit !== null) {
+      $limitString = " LIMIT {$this->limit}";
+    }
+
+    return "SELECT $select FROM $from $whereString $orderString $limitString;";
+  }
+
+}
+
+
+$query = new QueryBuilder();
+
+$query->select(['id', 'name'])
+  ->from('users')
+  ->whereName('Vasia')
+  ->whereAge('25')
+  ->orderBy('created_at')
+  ->limit(10);
+
+echo $query->getSql();
+
+
+
+
+
+
+
+
+
+
+
